1f63757c8dd08ce8fc36754f87100ac1e083ebe7
kazuki0157 <apokali0157@gmail.com>
2022-05-13 19:13:17 +0900

    gimeiインストール

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,16 @@
 FactoryBot.define do
   factory :user do
+    transient do
+      person { Gimei.name }
+    end
     nickname              {Faker::Name.initials(number: 4)}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
-    last_name             {'山田'}
-    first_name            {'太郎'}
-    last_name_kana        {'ヤマダ'}
-    first_name_kana       {'タロウ'}
+    last_name             {person.last.kanji}
+    first_name            {person.first.kanji}
+    last_name_kana        {person.last.katakana}
+    first_name_kana       {person.first.katakana}
     birthday              {Faker::Date.in_date_period}
   end
 end
