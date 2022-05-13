f90702991d95647bd55cb93baf170c737596a8ab
kazuki0157 <apokali0157@gmail.com>
2022-05-13 19:09:00 +0900

    Fakerインストール

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'ユーザー１'}
-    email                 {'test@example'}
-    password              {'1q1q1q'}
+    nickname              {Faker::Name.initials(number: 4)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name             {'山田'}
     first_name            {'太郎'}
     last_name_kana        {'ヤマダ'}
     first_name_kana       {'タロウ'}
-    birthday              {'1935-07-07'}
+    birthday              {Faker::Date.in_date_period}
   end
 end
