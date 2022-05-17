b0da7cbc896278b33e2857f8e67d1b5381611599
kazuki0157 <apokali0157@gmail.com>
2022-05-17 18:23:33 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :Category
+  belongs_to :Condition
+  belongs_to :Ken_name
+  belongs_to :Pay_for_shipping
+  belongs_to :Shiooing_days
 end
