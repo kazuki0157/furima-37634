aab9ea5fad16246f67668d86a711cd518b1ad9be
kazuki0157 <apokali0157@gmail.com>
2022-05-17 20:48:22 +0900

    バリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,14 @@
 class Item < ApplicationRecord
+  validates :image, presence: true
+  validates :product_name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :pay_for_shipping_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :ken_name_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :shipping_days_id, numericality: { other_than: 0, message: "can't be blank" } 
+  validates :price, numericality: { greater_than: 299,less_than: 10000000 }
+  
   has_one_attached :image
   belongs_to :user
   
