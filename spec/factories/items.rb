1121e0e481f966a072acedda324ba6bf52122752
kazuki0157 <apokali0157@gmail.com>
2022-05-18 15:06:32 +0900

    Itemモデルの単体テスト実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name        {Faker::Name.name}
+    description         {Faker::Lorem.sentence}
+    category_id         {Faker::Number.within(range: 1..10)}
+    condition_id        {Faker::Number.within(range: 1..6)}
+    pay_for_shipping_id {Faker::Number.within(range: 1..2)}
+    ken_name_id         {Faker::Number.within(range: 1..47)}
+    shipping_days_id    {Faker::Number.within(range: 1..3)}
+    price               {Faker::Number.within(range: 300..999999)}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
