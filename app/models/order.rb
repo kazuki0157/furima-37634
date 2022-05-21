a63192b03ec062909c6251f2d2e1c01276268473
kazuki0157 <apokali0157@gmail.com>
2022-05-21 21:00:07 +0900

    orderモデルバリデーション追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,8 @@
 class Order < ApplicationRecord
+  validates :zip, presence: true
+  validates :ken_name_id, presence: true
+  validates :city_name, presence: true
+  validates :address, presence: true
+  validates :tel, presence: true
+
 end
